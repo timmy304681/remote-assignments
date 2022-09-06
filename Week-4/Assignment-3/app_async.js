f498dcfe0b50933cd42054f311e4ae916c84c613
timmy304681 <timmy304681@gmail.com>
2022-09-06 10:16:46 +0800

    add async/await version

---
+++ Week-4/Assignment-3/app_async.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse application
+app.use(bodyParser.urlencoded({ extended: false }));
+// using pug
+app.set("view engine", "pug");
+
+// welcome page
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+/** Function **/
+
+// check email format
+function checkEmail(email) {
+  /* Email R egular Expression , reference: https://ithelp.ithome.com.tw/articles/10094951 */
+  const emailRule =
+    /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+  if (email.search(emailRule) != -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function insertSQL(sql, post) {
+  conn.query(sql, post, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+  });
+}
+
+/** main **/
+//Create db & table
+/* Notes
+If using XAMPP, set host: "192.168.64.2", can dump SQL by phpMyAdmin
+I still couldn't find the script (mysql,mysqldump) PATH about XAMPP in macos, so I couldn't use command line to solve everything I need
+Finally, I choose mySQL workbetch, because it's simple to use (already set in PATH)
+*/
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "yangtui",
+  password: "",
+});
+app.get("/createDBTable", async (req, res) => {
+  let sql_db = "CREATE DATABASE assignment;";
+  conn.query(sql_db, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+  });
+  const sql_table =
+    "CREATE TABLE assignment.user(id int AUTO_INCREMENT, email VARCHAR(255), password VARCHAR(255) , PRIMARY key(id));";
+  // Column id should be a primary key and increase automatically
+
+  conn.query(sql_table, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+  });
+
+  res.send("Posts DB and table created...");
+});
+
+// Sign-up , add account to MySQL
+app.post("/sign-up", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const emailValid = await checkEmail(email);
+
+  const sqlFind = `SELECT email FROM assignment.user WHERE email = "${email}"`;
+
+  conn.query(sqlFind, (err, results) => {
+    /* check three conditions
+    1. If the email is already in the database.
+    2. If the email is valid.
+    3. If the password input.
+    */
+    if (results.length === 0 && emailValid && password) {
+      // insert user email and passport to MySQL
+      const sql = "INSERT INTO assignment.user SET ?";
+      const post = {
+        email: `${email}`,
+        password: `${password}`,
+      };
+
+      insertSQL(sql, post);
+    } else {
+      const signUpError = true;
+      res.render("index", { signUpError });
+    }
+  });
+});
+
+// Sign-in , check if account is in MySQL
+app.post("/sign-in", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const sqlFind = `SELECT email FROM assignment.user WHERE email = "${email}" AND password ="${password}"`;
+
+  conn.query(sqlFind, (err, results) => {
+    if (results.length === 1) {
+      res.redirect("/member");
+    } else {
+      let signInError = true;
+      res.render("index", { signInError });
+    }
+  });
+});
+
+app.get("/member", (req, res) => {
+  res.render("member");
+});
+
+app.listen("3000", () => {
+  console.log("Server started  on port 3000");
+});
--- Week-4/Assignment-4/Assignment-4.js
+++ Week-4/Assignment-4/Assignment-4.js
@@ -1,6 +1,7 @@
 function delayedResultPromise(n1, n2, delayTime) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => console.log(n1 + n2), delayTime);
+    setTimeout(() => resolve(n1 + n2), delayTime);
+    //setTimeout(() => console.log(n1 + n2), delayTime);
   });
 }
 
@@ -9,7 +10,25 @@ delayedResultPromise(4, 5, 3000).then(console.log);
 
 async function main() {
   // your code here, you should call delayedResultPromise here and get the result using async/await.
-  await delayedResultPromise(4, 5, 3000).then(console.log);
+  console.log(await delayedResultPromise(4, 5, 3000));
 }
 
 main(); // result will be shown in the console after <delayTime> seconds
+
+/*** 
+ * Test
+我們起初應該是預期要依序執行此兩行，但是事實上第一行的promise會延遲才回傳，反而第二行會先
+也因此await就是為了解決此promise，能讓其完成執行結束後才進行下一個指令
+***/
+
+function test_wrong() {
+  delayedResultPromise(1, 2, 4000).then(console.log);
+  console.log("Finish!");
+}
+test_wrong();
+
+async function test_correct() {
+  console.log(await delayedResultPromise(1, 2, 4000));
+  console.log("Finish!");
+}
+test_correct();
